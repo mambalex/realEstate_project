74e18801c909bf26beb13b51dde10698b063a43a
mambalex <alexzhangzhiqin@gmail.com>
2019-11-28 23:28:00 +1100

    initial commit

---
+++ listings/choices.py
@@ -0,0 +1,36 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '500000':'$500,000',
+  '600000':'$600,000',
+  '700000':'$700,000',
+  '800000':'$800,000',
+  '900000':'$900,000',
+  '1000000':'$1M',
+  '1500000':'$1.5M',
+  '2000000':'$2M',
+  '2500000':'$2.5M',
+  '5000000':'$5M',
+}
+
+state_choices = {
+        'NSW': 'New South Wales',
+        'VIC': 'Victoria',
+        'QLD': 'Queensland',
+        'SA': 'South Australia',
+        'WA': 'Western Australia',
+        'ACT': 'Australian Capital Territory',
+        'NT': 'Northern Territory',
+        'TAS': 'Tasmania',
+}
